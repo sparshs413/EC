bb89b789191f207f2ba2a61b56354bd6bf163a1e
Sparsh Sihotiya <sparshs@iitk.ac.in>
2020-07-29 14:58:53 +0530

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,7 +10,7 @@
   // Preloader
   $(window).on('load', function() {
     if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function() {
+      $('#preloader').delay(0.000001).fadeOut('slow', function() {
         $(this).remove();
       });
     }
